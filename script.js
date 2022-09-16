68f318110b1b26249d9cfb3a8721d3200cf1c682
mfg25 <unnmfg@usp.br>
2022-09-16 06:05:46 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -1,63 +1,77 @@
 const boardCell = document.getElementsByClassName('board-cell')
 const form = document.getElementById('form')
 
-form.addEventListener('click', e =>{
+form.addEventListener('click', e => {
     e.preventDefault();
-    playGame(player1Name.value, player2Name.value);
+    createGame(player1Name.value, player2Name.value);
 })
 
 Array.from(boardCell).forEach(element => {
     element.dataset.index = Array.from(boardCell).indexOf(element);
-    element.addEventListener('click', e =>{
+    element.addEventListener('click', e => {
         receiveDataFromPlay(element.dataset.index)
     })
 
-    
+
 });
 
 let gameOn = false;
+let playerSelector = 'X'
 
-function createGame(namePlayer1,namePlayer2){
+function createGame(namePlayer1, namePlayer2) {
     let player1 = createPlayer(namePlayer1)
     let player2 = createPlayer(namePlayer2)
+    
     gameOn = true;
+    
+    
 }
 
-function receiveDataFromPlay(boardCellClicked){
-    console.log(boardCellClicked);
-    if(gameOn == true) playRound(boardCellClicked)
+function receiveDataFromPlay(boardCellClicked) {
+    
+    if (gameOn == true) playRound(boardCellClicked)
 }
-function showIconsOnTheBoard(player){
 
+function showIconsOnTheBoard(player, boardCellClicked) {
+    console.log(player)
+    Array.from(boardCell).forEach(element => {
+        if(element.dataset.index == boardCellClicked){
+            if(player == 'O'){
+                let image = document.createElement('img');
+                image.src = 'o.png'
+                element.appendChild(image)
+            }
+            if(player == 'X'){
+                let image = document.createElement('img');
+                image.src = 'x.png'
+                element.appendChild(image)
+            }
+        }
+    });
 }
+
 
 const playersTurn = () => {
     
-    let playerSelector = 'X'
-    return {
-        changePlayer: () =>{
-            if(playerSelector == 'O') playerSelector = 'X'
-            else playerSelector = 'O'
-            return playerSelector
-        }
-    }
-}
-function playRound(){
+    if (playerSelector == 'O') playerSelector = 'X'
+    else playerSelector = 'O'
+    
+    return playerSelector
 
 }
+function playRound(boardCellClicked) {
+    showIconsOnTheBoard(playersTurn(), boardCellClicked)
+}
 
-function createPlayer(name){
+function createPlayer(name) {
     let counter = 0;
     return {
         name,
-        incrementCounter: ()=> counter++,
+        incrementCounter: () => counter++,
         resetCounter: () => counter = 0,
     }
 }
 
 
-const newppp = playersTurn();
-console.log(newppp.changePlayer())
-console.log(newppp.changePlayer())
-console.log(newppp.changePlayer())
+
 
